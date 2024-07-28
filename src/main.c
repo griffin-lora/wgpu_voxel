cbf397a9acd2c25024753ca4bd4c5c36303fdfc9
griffin-lora <griffin-lora@github.com>
2024-07-28 18:02:23 -0400

    add frame waiting

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "camera.h"
+#include "chrono.h"
 #include "gfx/gfx.h"
 #include "result.h"
 #include <GLFW/glfw3.h>
@@ -10,7 +11,11 @@ int main() {
         return 1;
     }
 
+    microseconds_t program_start = get_current_microseconds();
+
+    float delta = 1.0f/60.0f;
     while (!glfwWindowShouldClose(window)) {
+        microseconds_t start = get_current_microseconds() - program_start;
         glfwPollEvents();
         camera_update();
 
@@ -18,7 +23,22 @@ int main() {
             print_result_error(result);
             return 1;
         }
+
+        microseconds_t end = get_current_microseconds() - program_start;
+        microseconds_t delta_microseconds = end - start;
+        delta = (float)delta_microseconds/1000000.0f;
+
+        microseconds_t remaining_microseconds = (1000000l/120l) - delta_microseconds;
+        if (remaining_microseconds > 0) {
+            sleep_microseconds(remaining_microseconds);
+        }
+
+        end = get_current_microseconds() - program_start;
+        microseconds_t new_delta_microseconds = end - start;
+        delta = (float)new_delta_microseconds/1000000.0f;
     }
+
+    (void)delta;
 
     term_gfx();
 
